2ee263c0b4ab60f39b8c5fc9c967cf4342f3f110
Anton Augustsson <anton.augustsson99@gmail.com>
2023-03-30 13:55:51 +0200

    main file for sensor-node and base-station

---
+++ base-station/main.c
@@ -0,0 +1,14 @@
+#include <zephyr/kernel.h>
+#include <stdio.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/drivers/sensor.h>
+
+
+#define SLEEP_TIME 3000
+
+int main(void)
+{
+    return 0;
+}
---
+++ sensor-node/main.c
@@ -0,0 +1,12 @@
+#include "pico/stdlib.h"
+#include "pico/util/queue.h"
+#include "hardware/sync.h"
+#include <stdio.h>
+#include "pico/time.h"
+#include "hardware/irq.h"
+#include "hardware/pwm.h"
+
+int main() 
+{
+    return 0;
+}
