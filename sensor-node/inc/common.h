ac455471eedb9fd37a41b4ab67b8af82b3c34491
McFrappe <obeyfranso@gmail.com>
2023-04-12 16:59:06 +0200

    chore: removed typedef line and placed it inline instead

--- sensor-node/inc/common.h
+++ sensor-node/inc/common.h
@@ -3,11 +3,10 @@
 
 #define ADC_CONVERSION_FACTOR ((const float)3.3f / (1 << 12))
 
-enum sensor_error_t {
+typedef enum sensor_error_t {
     SUCCESS,
     ERROR,
     NOT_READY,
-};
-typedef enum sensor_error_t error_t;
+} error_t ;
 
 #endif /* _COMMON_H_ */
