0dfb5a44b0faf3e08d20e57b96655f5280c069ab
Anton Augustsson <anton.augustsson99@gmail.com>
2023-04-13 19:41:24 +0200

    fixed proper reg values for temp

--- sensor-node/tests/mocks.c
+++ sensor-node/tests/mocks.c
@@ -5,21 +5,8 @@ int reg_read;
 
 int i2c_write_blocking(int i2c, int addr, uint8_t *reg, size_t len, bool option)
 {
-    if (*reg == BME680_TEMP_PAR_T1_LSB || 
-        *reg == BME680_TEMP_PAR_T1_MSB || 
-        *reg == BME680_TEMP_PAR_T2_LSB || 
-        *reg == BME680_TEMP_PAR_T2_MSB || 
-        *reg == BME680_TEMP_PAR_T3)
-    {
-        reg_read = *reg;
-        return 1;
-    }
-    else 
-    {
-        // TODO: can add accrual write hear
-        reg_read = *reg;
-        return 1;
-    }
+    reg_read = *reg;
+    return 1;
 }
 
 int i2c_read_blocking(int i2c, int addr, uint8_t *buf, size_t len, bool option)
@@ -27,18 +14,33 @@ int i2c_read_blocking(int i2c, int addr, uint8_t *buf, size_t len, bool option)
     switch(reg_read){    
         case BME680_TEMP_PAR_T1_LSB:    
             buf[0] = 243;
+            break;
         case BME680_TEMP_PAR_T1_MSB:    
             buf[0] = 101;
+            break;
         case BME680_TEMP_PAR_T2_LSB:    
             buf[0] = 85;
+            break;
         case BME680_TEMP_PAR_T2_MSB:    
             buf[0] = 103;
+            break;
         case BME680_TEMP_PAR_T3: 
             buf[0] = 3;
+            break;
         case BME680_TEMP_ADC_MSB: 
+            if (len != 3)
+                break;
+            
             buf[0] = 122;
             buf[1] = 125;
             buf[2] = 144;
+            break;
+        default:
+            for (int i = 0; i < len; i++)
+            {
+               buf[i] = 1;
+            }
+            break;
     }
 
 
