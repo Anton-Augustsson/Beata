e30ecd1570a31456978e357b04182313789c0130
McFrappe <obeyfranso@gmail.com>
2023-04-17 14:08:52 +0200

    Added small template for preperation to implement the basenode driver.
    Co-authored-by: Fredrik Engstrand <fredrik@engstrand.nu>
    Co-authored-by: Anton Augustsson <Anton-Augustsson@users.noreply.github.com>

--- base-station/src/main.c
+++ base-station/src/main.c
@@ -1,14 +1,17 @@
-#include <zephyr/kernel.h>
 #include <stdio.h>
+
+#include "../inc/basestation.h"
+
+#include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/sensor.h>
 
-
 #define SLEEP_TIME 3000
 
-int main(void)
+int
+main()
 {
     return 0;
 }
