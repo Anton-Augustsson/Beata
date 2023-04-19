a381b3bd87bfd6e5d8f52ec7730e1dceb226a22e
McFrappe <obeyfranso@gmail.com>
2023-04-19 16:48:24 +0200

    Began incorporating the beata sensor and base station
    Co-authored-by: Anton Augustsson <Anton-Augustsson@users.noreply.github.com>
        Co-authored-by: Fredrik Engstrand <fredrik@engstrand.nu>

--- base-station/src/main.c
+++ base-station/src/main.c
@@ -1,51 +1,42 @@
+#include <sensor/beata/beata.h>
 #include <stdio.h>
-#include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/drivers/sensor.h>
-
-#include <sensor/beata/beata.h>
+#include <zephyr/kernel.h>
 
 #define SLEEP_TIME 3000
-
-// TODO:
-// void
-// print_sensor_value(struct sensor_readings sensor)
-// {
-//     printf("============ SENSOR READINGS ==============\n");
-//     printf("Humidity (%%): %d.%02d\n", sensor.humidity.data / HUM_RESOLUTION,
-// 	   (sensor.humidity.data % HUM_RESOLUTION) * 1000);
-//     printf("Pressure (hPa): %d.%02d\n", sensor.press.data / PRESS_RESOLUTION,
-// 	   sensor.press.data % PRESS_RESOLUTION);
-//     printf("Temperature (C): %d.%02d\n",
-// 	   sensor.temp_celsius.data / TEMP_RESOLUTION,
-// 	   sensor.temp_celsius.data % TEMP_RESOLUTION);
-//     printf("Has motion (bool): %d\n", sensor.has_motion.data);
-//     printf("Sound level: %d\n", sensor.sound_level.data);
-//     printf("===========================================\n\n");
-// }
-
 
 int
 main()
 {
     const struct device *const dev = DEVICE_DT_GET_ONE(beata);
-    struct sensor_value temp, humidity, pressure, motion, sound;
+    struct sensor_value temperature, humidity, pressure, motion, sound;
 
-    if (!device_is_ready(dev))
-    {
-		printk("sensor: device not ready.\n");
-		return 0;
-	}
+    if (!device_is_ready(dev)) {
+        printk("sensor: device not ready.\n");
+        return 0;
+    }
 
-	for(;;)
-    {
-		sensor_sample_fetch(dev);
-		sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temp);
+    for (;;) {
+        sensor_sample_fetch(dev);
+        sensor_channel_get(dev, SENSOR_CHAN_AMBIENT_TEMP, &temperature);
+        sensor_channel_get(dev, SENSOR_CHAN_HUMIDITY, &humidity);
+        sensor_channel_get(dev, SENSOR_CHAN_PRESS, &pressure);
+        sensor_channel_get(dev, SENSOR_CHAN_SOUND, &sound);
+        sensor_channel_get(dev, SENSOR_CHAN_IR, &motion);
 
-        printk("Value: %d.%d\n", temp.val1, temp.val2);
+        printf("============ SENSOR READINGS ==============\n");
+        printf("Humidity (%%): %d.%02d\n", humidity.val1, humidity.val2);
+        printf("Pressure (hPa): %d.%02d\n", pressure.val1, pressure.val2);
+        printf("Temperature (C): %d.%02d\n", temperature.val1, temperature.val2);
+        printf("Has motion (bool): %d\n", motion.val1);
+        printf("Sound level (dB): %d\n", sound.val1);
+        printf("===========================================\n\n");
+
         k_msleep(SLEEP_TIME);
-	}
+    }
+
     return 0;
 }
