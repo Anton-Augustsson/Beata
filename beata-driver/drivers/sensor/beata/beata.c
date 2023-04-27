d98a26c3dada2d61f1b20d8b19773526a509f0d9
McFrappe <obeyfranso@gmail.com>
2023-04-27 16:39:13 +0200

    fix: Forgot about break statements

--- beata-driver/drivers/sensor/beata/beata.c
+++ beata-driver/drivers/sensor/beata/beata.c
@@ -11,7 +11,7 @@ static int
 beata_sample_fetch(const struct device *dev, enum sensor_channel chan)
 {
 	struct beata_data *data = dev->data;
-	const struct beata_config *config = dev->config;
+	struct beata_config *config = dev->config;
 	i2c_burst_read_dt(
 		&config->i2c, SENSOR_NODE_HUM, &data->humidity, 4);
 	i2c_burst_read_dt(
@@ -31,23 +31,28 @@ beata_channel_get(const struct device *dev, enum sensor_channel chan, struct sen
 
     switch (chan)
     {
-	case SENSOR_CHAN_AMBIENT_TEMP:
-		val->val1 = data->temp_celsius / 100;
-		val->val2 = data->temp_celsius % 100;
-	case SENSOR_CHAN_HUMIDITY:
-		val->val1 = data->humidity / 1000;
-		val->val2 = data->humidity % 1000;
-	case SENSOR_CHAN_PRESS:
-		val->val1 = data->press / 1000;
-		val->val2 = data->press % 1000;
-	case SENSOR_CHAN_IR:
-		val->val1 = data->has_motion;
-		val->val2 = 0;
-	case SENSOR_CHAN_PROX:
-		val->val1 = data->sound_level / 100;
-		val->val2 = data->sound_level % 100;
-	default:
-	    return -EINVAL;
+		case SENSOR_CHAN_AMBIENT_TEMP:
+			val->val1 = data->temp_celsius / 100;
+			val->val2 = data->temp_celsius % 100;
+			break;
+		case SENSOR_CHAN_HUMIDITY:
+			val->val1 = data->humidity / 1000;
+			val->val2 = data->humidity % 1000;
+			break;
+		case SENSOR_CHAN_PRESS:
+			val->val1 = data->press / 1000;
+			val->val2 = data->press % 1000;
+			break;
+		case SENSOR_CHAN_IR:
+			val->val1 = data->has_motion;
+			val->val2 = 0;
+			break;
+		case SENSOR_CHAN_PROX:
+			val->val1 = data->sound_level / 100;
+			val->val2 = data->sound_level % 100;
+			break;
+		default:
+			return -EINVAL;
     }
 
     return 0;
